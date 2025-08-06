c8a4483e10224e4a43f5a0896df344d3d1b3cc08
VerBytesnBits <oliverreroma@gmail.com>
2025-08-06 17:23:02 +0800

    Assigning room feature

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -6,26 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-/**
- * @property int $id
- * @property string $name
- * @property int|null $lab_in_charge_id
- * @property string $status
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Models\User|null $labInCharge
- * @method static \Database\Factories\RoomFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room query()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room whereLabInChargeId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room whereStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Room whereUpdatedAt($value)
- * @mixin \Eloquent
- */
+
 class Room extends Model
 {
     /** @use HasFactory<\Database\Factories\RoomFactory> */
